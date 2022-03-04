4f89532daccc76716fb5bbb4966c2743b561b040
Ganjikunta <subba.ganjikunta@delta.com>
2022-03-04 14:08:21 -0500

    initial commit

---
+++ src/main/java/com/poc/externalconfig/ConfigexternalizationApplication.java
@@ -0,0 +1,13 @@
+package com.poc.externalconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigexternalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigexternalizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/externalconfig/ConfigexternalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.externalconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigexternalizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
